d8ef690ddf5d4d439ed39e17d7b981a5e06d083e
kunal kumar <105341082+kunalkasera456@users.noreply.github.com>
2023-06-20 15:23:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+ let colorChoose = document.getElementByid("colorSelect");
+ let removeColor = document.getElementsByTagName("input")[0];
+
+removeColor.eventListner("click", function () {
+	// Get the index of the selected option
+  const selectedIndex = colorSelect.selectedIndex;
+
+  // Remove the selected option from the dropdown
+  colorSelect.remove(selectedIndex);
+	
+})
